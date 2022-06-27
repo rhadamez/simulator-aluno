a31190b7344fe17841f5d3ec96fb337de4b66bff
Rhadamez Gindri Hercilio <rhadamez@gmail.com>
2022-06-27 11:16:08 +0000

    feat: add godotenv

--- go.mod
+++ go.mod
@@ -2,4 +2,7 @@ module github.com/rhadamez/simulator-aluno
 
 go 1.18
 
-require github.com/confluentinc/confluent-kafka-go v1.9.0
+require (
+	github.com/confluentinc/confluent-kafka-go v1.9.0
+	github.com/joho/godotenv v1.4.0
+)
--- main.go
+++ main.go
@@ -2,9 +2,18 @@ package main
 
 import (
 	"fmt"
+	"log"
 
+	"github.com/joho/godotenv"
 	"github.com/rhadamez/simulator-aluno/application/route"
 )
+
+func init() {
+	err := godotenv.Load()
+	if err != nil {
+		log.Fatal("error loading .env file")
+	}
+}
 
 func main() {
 	route := route.Route{
